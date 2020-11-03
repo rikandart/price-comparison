13812498fd274bc138903a1b1039abd9f8af6619
rikandart <pizhun97@gmail.com>
2020-11-03 18:18:55 +0300

    initial commit

---
+++ src/test/java/com/example/pricecomparison/PriceComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pricecomparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
